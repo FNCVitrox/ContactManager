a096717dae2038182950d16fab6f221a232d3ec5
Vesel Demi <121579486+FNCVitrox@users.noreply.github.com>
2025-07-13 23:30:53 +0200

    Add files via upload

---
+++ Contact Manager/AddMitarbeiter.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Contact_Manager
+{
+    public partial class AddMitarbeiter : UserControl
+    {
+        private readonly string filePath = "mitarbeiter.json";
+        public AddMitarbeiter()
+        {
+            InitializeComponent();
+            cmbAnrede.DataSource = Enum.GetValues(typeof(Anrede));
+            cmbGeschlecht.DataSource = Enum.GetValues(typeof(Geschlecht));
+            cmbStatus.DataSource = Enum.GetValues(typeof(Status));
+
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbAnrede_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cmbGeschlecht_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        private void txtbVorname_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void cmbStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSpeichern_Click(object sender, EventArgs e)
+        {
+            Mitarbeiter mitarbeiter = new Mitarbeiter
+            {
+           
+
+
+            };
+            List<Mitarbeiter> liste = new List<Mitarbeiter>();
+
+            if (File.Exists(filePath))
+            {
+                string existingJson = File.ReadAllText(filePath);
+                liste = JsonConvert.DeserializeObject<List<Mitarbeiter>>(existingJson) ?? new List<Mitarbeiter>();
+            }
+
+            liste.Add(mitarbeiter);
+
+            string json = JsonConvert.SerializeObject(liste, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+
+            MessageBox.Show("Mitarbeiter gespeichert!");
+        }
+        // 3. Zur Liste hinzufügen
+        // mitarbeiterListe.Add(mitarbeiter);
+
+        // 4. Optional: Feedback geben
+        // MessageBox.Show("Mitarbeiter erfolgreich gespeichert.");
+
+        // 5. Eingabefelder zurücksetzen (optional)
+        // LeereFormular();
+
+        private void txtbNachname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddMitarbeiter_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ Contact Manager/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Contact_Manager
+{
+    public enum Geschlecht
+    {
+        Männlich,
+        Weiblich
+    }
+
+    public enum Status
+    {
+        Aktiv,
+        Passiv
+    }
+
+    public enum Anrede
+    {
+        Herr,
+        Frau
+    }
+    public abstract class Person
+    {
+        // Persoenliche Daten
+        public Anrede Anrede { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public DateTime Geburtsdatum { get; set; }
+        public Geschlecht Geschlecht { get; set; }
+        public string Titel { get; set; }
+
+        // Kontaktinformationen
+        public string TelefonGeschaeft { get; set; }
+        public string Mobiltelefon { get; set; }
+        public string TelefonPrivat { get; set; }
+        public string Email { get; set; }
+        public Status Status { get; set; }
+
+        // Adressdaten
+        public string Wohnort { get; set; }
+        public string Nationalitaet { get; set; }
+        public string Adresse { get; set; }
+        public string Postleitzahl { get; set; }
+    }
+}
